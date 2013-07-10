47bde82d8d3cb60c15e53706d979e16573cff4fa
Jorge García <jgarciao@gmail.com>
2013-07-10 23:07:13 +0200

    Add index.html file and modify web.js to return index.html contents

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var file = "index.html"
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var buffer = fd.readFileSync(file);
+  response.send(buffer.toString());
+
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
