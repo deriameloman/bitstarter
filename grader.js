375b07d3f3cd15bef286f27b847bacb02d6408a7
Jorge García <jgarciao@gmail.com>
2013-07-12 00:36:38 +0200

    Update grader.js to take either a file or a URL as an input on the command line

--- grader.js
+++ grader.js
@@ -19,13 +19,25 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ + restler
+    - https://github.com/danwrong/restler
+
+ + util
+    - http://nodejs.org/docs/latest/api/util.html   
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var util = require('util');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "http://morning-garden-1878.herokuapp.com/"
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var TEMPFILE = "temp.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,9 +48,35 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(inurl) {
+    var instr = inurl.toString();
+    if(instr.length == 0) {
+        console.log("Empty url. Exiting.");
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var checkUrlAndOutJson = function(url, checks) {    
+
+  rest.get(url).on('complete', function(result) {
+    if (result instanceof Error) {
+      util.puts('Error: ' + result.message);
+      this.retry(5000); // try again after 5 sec
+    } else {           
+      fs.writeFileSync(TEMPFILE, result);                        
+      var checkJson = checkHtmlFile(TEMPFILE, checks);         
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+  });
+
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -61,14 +99,25 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to check', clone(assertUrlExists))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if (program.url)  {
+      //console.log("Checking %s", program.url);
+      checkUrlAndOutJson(program.url, program.checks);          
+    }
+    else{
+      //console.log("Checking %s", program.file);
+      var checkJson = checkHtmlFile(program.file, program.checks);         
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }  
+         
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
